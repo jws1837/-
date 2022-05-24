180e0480120d7c4e23fc5b1fb9718a451406f5ab
jws1837 <jws1837@gmail.com>
2022-05-24 16:36:41 +0900

    세자리 난수 수정중 오류나서 임시 로직으로 백업

--- src/main/java/com/flab/baseballgame/service/Utils.java
+++ src/main/java/com/flab/baseballgame/service/Utils.java
@@ -1,24 +1,17 @@
 package com.flab.baseballgame.service;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class Utils {
-    public static void main(String[] args) {
-        System.out.println(getThreeRandumNumber());
-    }
-    public static int getThreeRandumNumber() {
-        int temp[] = new int[3];
-        temp[0] = new Random().nextInt(9);
-        for (int i = 1; i < 3; i++) {
-            temp[i] = new Random().nextInt(9);
-            if (temp[i] == temp[i + 1]) {
-                new Random().nextInt(9);
+        public static int getThreeRandumNumber() {
+            int number = new Random().nextInt(999);
+            for (; ; ) {
+                if (number < 100) {
+                    number = new Random().nextInt(999);
+                } else {
+                    break;
+                }
             }
+            return number;
         }
-        Arrays.toString(temp).replaceAll("[^0-9]","");
-       int number =  Integer.parseInt(Arrays.toString(temp));
-
-        return number;
-    }
 }
